bd2ef2dddc93cef8f10df26f6e03e39ed2868925
John <jpfurtak@gmail.com>
2016-02-26 12:44:58 -0800

    adding basic_eapi.py

---
+++ basic_eapi.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+from jsonrpclib import Server
+from pprint import pprint as pp
+
+switch = Server('http://jfurtak:arista123@192.168.56.10/command-api')
+output = switch.runCmds(1, ['show version'])
+pp(output[0]['systemMacAddress'])
+result = switch.runCmds(1, ['enable',
+                            'configure',
+                            'interface Management1',
+                            "description MAC: %s" % output[0]
+['systemMacAddress']])
